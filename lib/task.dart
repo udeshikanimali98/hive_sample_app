afab528326264aa4c4c855ffa4357249f29ad072
udeshikanimali98 <udeshikanimali644@gmail.com>
2025-06-07 10:03:08 +0530

    remove commentd codes

--- lib/task.dart
+++ lib/task.dart
@@ -1,19 +1,3 @@
-// import 'package:hive/hive.dart';
-
-// part 'task.g.dart';
-
-// @HiveType(typeId: 0)
-// class Task extends HiveObject {
-//   @HiveField(0)
-//   String title;
-
-//   @HiveField(1)
-//   bool isDone;
-
-//   Task({required this.title, this.isDone = false});
-// }
-
-
 import 'package:hive/hive.dart';
 
 part 'task.g.dart';
